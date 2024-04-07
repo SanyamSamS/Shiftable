88ebbd434db163c5c6c24e97f06ba288bbd048e2
JohnM89 <mjammer2@gmail.com>
2024-04-07 15:50:53 -0400

    added connection.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
-// const cors = require('cors');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
