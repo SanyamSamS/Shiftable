cfb001e3019357cf067e1c4e7ec9dd253077b183
CYCBrian <143226784+CYCBrian@users.noreply.github.com>
2024-04-09 17:49:55 -0400

    ADDED Basic Form, gonna try adding loading state, form validation, error handling messages to client, and maybe a protected route?

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -1,19 +1,58 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-
-// note created yet //
-import { useAuth } from "../src/util/useAuth";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../api";
 
 const LoginForm = () => {
+  const [userFormData, setUserFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await loginUser(userFormData);
+      const { token } = response.data;
+      localStorage.setItem("token", token);
+      navigate("/dashboard");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <section className="login-component">
       <form className="login-form" onSubmit={handleFormSubmit}>
         <section className="input-container">
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
+          <input
+            className="input-field"
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Username"
+            onChange={handleInputChange}
+            value={userFormData.username}
+          />
+          <input
+            className="input-field"
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            onChange={handleInputChange}
+            value={userFormData.password}
+          />
         </section>
         <section className="button-container">
-          <button type="submit">Log In</button>
+          <button className="submit-button" type="submit">
+            Log In
+          </button>
         </section>
       </form>
     </section>
