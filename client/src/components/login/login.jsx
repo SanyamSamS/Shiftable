beab26a330cbfc9bc887dedd9bc6646e7e227346
CYCBrian <143226784+CYCBrian@users.noreply.github.com>
2024-04-10 17:22:36 -0400

    ADDED loading state, error handling messages to client. Form validation might be uneccessary unless organization has predefined username and password format.

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -3,31 +3,54 @@ import { useNavigate } from "react-router-dom";
 import { loginUser } from "../api";
 
 const LoginForm = () => {
-  const [userFormData, setUserFormData] = useState({
+  const [employeeCredentials, setEmployeeCredentials] = useState({
     username: "",
     password: "",
   });
+  const [loginAlert, setLoginAlert] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   const navigate = useNavigate();
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
+    setEmployeeCredentials({ ...employeeCredentials, [name]: value });
   };
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    setLoading(true);
+
     try {
-      const response = await loginUser(userFormData);
+      const response = await loginUser(employeeCredentials);
       const { token } = response.data;
       localStorage.setItem("token", token);
       navigate("/dashboard");
     } catch (err) {
-      console.log(err);
+      if (err.response) {
+        if (err.response.status === 404) {
+          setLoginAlert("User not found. Please check your credentials.");
+          setEmployeeCredentials({ ...employeeCredentials, password: "" });
+          return;
+        } else if (err.response.status === 401) {
+          setLoginAlert("Incorrect password. Please try again.");
+          setEmployeeCredentials({ ...employeeCredentials, password: "" });
+          return;
+        } else {
+          console.log(err);
+          setLoginAlert("An error occurred. Please try again later.");
+          setEmployeeCredentials({ ...employeeCredentials, password: "" });
+          return;
+        }
+      }
+    } finally {
+      setLoading(false)
     }
   };
 
   return (
     <section className="login-component">
+      <h2>Employee Log In</h2>
       <form className="login-form" onSubmit={handleFormSubmit}>
         <section className="input-container">
           <input
@@ -37,7 +60,7 @@ const LoginForm = () => {
             name="username"
             placeholder="Username"
             onChange={handleInputChange}
-            value={userFormData.username}
+            value={employeeCredentials.username}
           />
           <input
             className="input-field"
@@ -46,14 +69,22 @@ const LoginForm = () => {
             name="password"
             placeholder="Password"
             onChange={handleInputChange}
-            value={userFormData.password}
+            value={employeeCredentials.password}
           />
         </section>
         <section className="button-container">
-          <button className="submit-button" type="submit">
-            Log In
+          <button
+            className="submit-button"
+            type="submit"
+            disabled={!(employeeCredentials.username && employeeCredentials.password)}>
+            {loading ? "Logging in ..." : "Log in"}
           </button>
         </section>
+        {loginAlert && (
+          <div className="login-alert" role="alert">
+            {loginAlert}
+          </div>
+        )}
       </form>
     </section>
   );
