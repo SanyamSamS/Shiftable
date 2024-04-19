cef865dbdbfd5de2f5e9d635e9b7812b6cb1a562
CYCBrian <143226784+CYCBrian@users.noreply.github.com>
2024-04-19 15:12:40 -0400

    Corrected classname in App.css, imported to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import LandingPage from '../src/components/landingpage/landingpage';
 import EmployeeDashboard from '../src/components/dashboard/employeeDashboard';
+import "./App.css";
 
 function App() {
   return (
